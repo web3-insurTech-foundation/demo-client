f4753f5b4494b8a1a19f4abc82d9fd96ea0d871d
Andrew W. Benson <awbenson@gmail.com>
2024-12-30 13:27:22 -0500

    AWB1- Fixing NPM Install and running

--- src/app/demo/components/apps/apps-routing.module.ts
+++ src/app/demo/components/apps/apps-routing.module.ts
@@ -3,7 +3,6 @@ import { RouterModule } from '@angular/router';
 
 @NgModule({
     imports: [RouterModule.forChild([
-        { path: 'calendar', data: { breadcrumb: 'Calendar' }, loadChildren: () => import('./calendar/calendar.app.module').then(m => m.CalendarAppModule) },
         { path: 'tasklist', data: { breadcrumb: 'Task List' }, loadChildren: () => import('./tasklist/tasklist.app.module').then(m => m.TaskListAppModule) },
         { path: 'chat', data: { breadcrumb: 'Chat' }, loadChildren: () => import('./chat/chat.app.module').then(m => m.ChatAppModule) },
         { path: 'files', data: { breadcrumb: 'Files' }, loadChildren: () => import('./file/file.app.module').then(m => m.FileAppModule) },
